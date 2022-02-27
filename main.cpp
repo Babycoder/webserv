94e895fb6342e8b57a1e0ce808e968bea1918f5c
Aymen Ghazali <ayghazal@e2r9p9.1337.ma>
2022-02-27 20:01:08 +0100

    fstream

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "parser.hpp"
 #include "request_parser.hpp"
 #include "server_parser.hpp"
+#include <fstream>
+#include <string>
+#include <iostream>
 
 
 int     main(int ac , char *av[])
@@ -8,7 +11,24 @@ int     main(int ac , char *av[])
 
     if (ac == 2)
     {
-        std::cout << "All Good (y)" << std::endl;
+        std::fstream file;
+        std::string line;
+
+        file.open(av[1], std::ios::in);
+        if(file.is_open())
+        {
+            while (getline(file, line))
+            {
+                std::cout << line << std::endl;
+            }
+            file.close();
+        }   
+        else
+        {
+            std::cerr << "Please enter a valid config file" << std::endl;
+            return 1;
+        }
+
     }
     else
     {
@@ -17,3 +37,6 @@ int     main(int ac , char *av[])
     }
     return 0;
 }
+
+
+
